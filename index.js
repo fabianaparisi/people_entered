41f5ca26371562edac767835b9a00edc8121ba91
Fabiana Parisi <fabiana.parisi@huumun.com>
2021-07-13 15:50:45 +0200

    js exercise

---
+++ index.js
@@ -0,0 +1,77 @@
+// document.getElementById("count-el").innerText = 5
+
+//let count = 5 + 7
+
+//console.log(count)
+
+//let myAge = 32
+
+//let humanDogRatio = 7
+
+//let myDogAge = myAge * humanDogRatio
+
+//console.log (myDogAge)
+
+//let bonusPoints = 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints = 45
+//console.log(bonusPoints)
+
+//function increment() {
+    //console.log("the button was clicked")
+//}
+
+//function countdown() {
+    //console.log(5)
+    //console.log(4)
+   // console.log(3)
+   // console.log(2)
+   // console.log(1)
+//}
+//countdown()
+
+
+//function myLogger() {
+    //console.log(42)
+//}
+//myLogger()
+
+//let lap1 = 34
+//let lap2 = 33
+//let lap3 = 36
+
+//function logLapTime() {
+    //let totalTime = lap1 + lap2 + lap3
+    //console.log(lap1 + lap2 + lap3)
+//}
+//logLapTime()
+
+//let lapsCompleted = 0
+//function incrementLap() {
+    //lapsCompleted = lapsCompleted + 1
+//}
+//incrementLap()
+//incrementLap()
+//incrementLap()
+
+//console.log(lapsCompleted)
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment (){
+    //console.log("clicked")
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
